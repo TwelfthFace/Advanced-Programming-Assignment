610467ad9920183093b938ed0bdf7b7eb2ba3342
TwelfthFace <alcomeho@gmail.com>
2021-10-15 15:36:20 +0100

    Improved GUI functionality and added layout implementation.

--- Advanced Programming Assignment/Form1.cs
+++ Advanced Programming Assignment/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,7 +16,32 @@ namespace Advanced_Programming_Assignment
         public Form1()
         {
             InitializeComponent();
+            this.Width = 1000;
+            this.Height = 850;
         }
+
+
+        //Keep Layout Aligned and Neat on repaint.
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            this.SetAndFillClip(e);
+        }
+
+
+        //sets graphics clipsize.
+        private void SetAndFillClip(PaintEventArgs e)
+        {
+
+            // Set the Clip property to a new region.
+            e.Graphics.Clip = new Region(new Rectangle(this.textBox1.Width+50, 13, this.Width-540, this.Height-80));
+
+            // Fill the region.
+            e.Graphics.FillRegion(Brushes.LightSalmon, e.Graphics.Clip);
+
+        }
+
 
     }
 }
