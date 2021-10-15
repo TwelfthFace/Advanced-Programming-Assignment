f7a1452142d467ce312ad32a6dbee9ab96f92a4d
TwelfthFace <alcomeho@gmail.com>
2021-10-15 10:45:55 +0100

    Added GUI components to form

--- Advanced Programming Assignment/Form1.Designer.cs
+++ Advanced Programming Assignment/Form1.Designer.cs
@@ -29,13 +29,67 @@ namespace Advanced_Programming_Assignment
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.panelGraphics = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(12, 411);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(576, 27);
+            this.textBoxInput.TabIndex = 0;
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(594, 410);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(94, 29);
+            this.btnRun.TabIndex = 1;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(694, 410);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(94, 29);
+            this.btnCheck.TabIndex = 2;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            // 
+            // panelGraphics
+            // 
+            this.panelGraphics.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.panelGraphics.Location = new System.Drawing.Point(12, 4);
+            this.panelGraphics.Name = "panelGraphics";
+            this.panelGraphics.Size = new System.Drawing.Size(776, 400);
+            this.panelGraphics.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panelGraphics);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.textBoxInput);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Panel panelGraphics;
     }
 }
 
