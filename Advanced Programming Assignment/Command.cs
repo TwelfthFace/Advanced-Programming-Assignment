6ed4c543d7ccc7a9b96d9326f9082498c07f4a22
TwelfthFace <alcomeho@gmail.com>
2021-10-17 10:43:10 +0100

    expanded drawing feautres and added error feedback

--- Advanced Programming Assignment/Command.cs
+++ Advanced Programming Assignment/Command.cs
@@ -9,19 +9,17 @@ namespace Advanced_Programming_Assignment
      //class to define commands and facilitate appropriate feature expansion.
     class Command
     {
+        protected Draw draw;
         protected Graphics graphicsContext;
         protected RectangleF clipbound;
         protected System.Windows.Forms.TextBox txtCmdLine;
         protected Pen penColor = new Pen(Color.Black, 3);
-        
-        public Command()
-        {
 
-        }
 
         public Command(System.Windows.Forms.TextBox txtCmdLine)
         {
             this.txtCmdLine = txtCmdLine;
+            this.draw = new Draw(graphicsContext);
         }
 
         public Command(System.Windows.Forms.TextBox txtCmdLine, Graphics graphics)
@@ -29,6 +27,7 @@ namespace Advanced_Programming_Assignment
             this.txtCmdLine = txtCmdLine;
             this.graphicsContext = graphics;
             this.clipbound = graphicsContext.ClipBounds;
+            this.draw = new Draw(graphicsContext);
         }
 
         public string TxtCmd
@@ -36,37 +35,65 @@ namespace Advanced_Programming_Assignment
             get => txtCmdLine.Text;
         }
 
-        public void Parser()
+        public Boolean Parser(System.Windows.Forms.ListBox errout)
         {
             string command = this.TxtCmd.Trim().ToLower();
             string[] split = command.Split(" ");
-            int commandParameter = 0;
+            string[] commandParameter = {"", ""};
             try { 
             
-                if (split.Length > 2)
+                if (split.Length > 3)
                 {
-                    Console.WriteLine("Error!");
+                    errout.Items.Insert(0, "Too many parameters! Max parameters 2.");
                 }
                 if (split.Length == 2)
                 {
-                    commandParameter = Int32.Parse(split[1]);
+                    commandParameter[0] = split[1];
+                }
+
+                if (split.Length == 3)
+                {
+                    commandParameter[0] = split[1];
+                    commandParameter[1] = split[2];
                 }
 
                 switch (split[0])
                 {
                     case "rectangle":
-                        new Draw(this.graphicsContext).DrawRectangle(0, 0, commandParameter, commandParameter, this.penColor.Color);
+                        if(split.Length < 3)
+                        {
+                            errout.Items.Insert(0, "Missing parameters! [rectangle w h]");
+                            return false;
+                        }
+                        draw.DrawRectangle(Int32.Parse(commandParameter[0]), Int32.Parse(commandParameter[1]), this.penColor.Color);
                         break;
                     case "circle":
-                        new Draw(this.graphicsContext).DrawCircle(commandParameter);
+                        if (split.Length < 2)
+                        {
+                            errout.Items.Insert(0, "Missing parameters! [circle c]");
+                            return false;
+                        }
+                        draw.DrawCircle(Int32.Parse(commandParameter[0]));
                         break;
+                    case "moveto":
+                        if (split.Length < 3)
+                        {
+                            errout.Items.Insert(0, "Missing parameters! [moveto x y]");
+                            return false;
+                        }
+                        draw.moveTo(Int32.Parse(commandParameter[0]), Int32.Parse(commandParameter[1]));
+                        break;
+                    default:
+                        errout.Items.Insert(0, "Unknown Command!");
+                        return false;
                 }
-            }catch(Exception e)
+            } catch (Exception e)
             {
-                Console.WriteLine(e.Message);
+                errout.Items.Insert(0, e.Message);
             }
-
+            return false;
         }
 
+        public Command() { }
     }
 }
--- Advanced Programming Assignment/Draw.cs
+++ Advanced Programming Assignment/Draw.cs
@@ -6,22 +6,45 @@ using System.Text;
 namespace Advanced_Programming_Assignment
 {
     class Draw : Command
-    { 
+    {
+
+        private Turtle turtle;
+
+        struct Turtle
+        {
+            public int x;
+            public int y;
+            public Pen turtleColour;
+
+        }
 
         public Draw(Graphics g)
         {
             this.graphicsContext = g;
             this.clipbound = g.ClipBounds;
+
+            turtle = new Turtle
+            {
+                x = 0,
+                y = 0,
+                turtleColour = this.penColor
+            };
         }
 
-        public void DrawRectangle(int x, int y, int width, int length, Color colour, int penSize = 3)
+        public void moveTo(int x, int y)
         {
-            graphicsContext.DrawRectangle(penColor, ((this.clipbound.X + penSize) - 1) + x, y + penSize + 12, width, length);
+            turtle.x = x;
+            turtle.y = y;
+        }
+
+        public void DrawRectangle(int width, int length, Color colour, int penSize = 3)
+        {
+            graphicsContext.DrawRectangle(penColor, ((this.clipbound.X + penSize) - 1) + turtle.x, turtle.y + penSize + 12, width, length);
         }
 
         public void DrawCircle(int radius)
         {
-            Rectangle rect = new Rectangle((int)clipbound.X, 20, radius, radius);
+            Rectangle rect = new Rectangle((int)clipbound.X + turtle.x, 20, radius, radius);
             this.graphicsContext.DrawEllipse(penColor, rect);
         }
     }
