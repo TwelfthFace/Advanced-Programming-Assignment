a95da119a50b05180e137670c5c5c1c1f3803006
TwelfthFace <alcomeho@gmail.com>
2021-10-21 15:25:17 +0100

    script engine implemented, can now run multiple commands at once using the txtscriptbox element and run script button.

--- Advanced Programming Assignment/Script.cs
+++ Advanced Programming Assignment/Script.cs
@@ -1,11 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace Advanced_Programming_Assignment
 {
     class Script : Command
     {
-       
+
+        public Script(Graphics graphics)
+        {
+            this.graphicsContext = graphics;
+            this.draw = new Draw(graphics);
+        }
+
+        public new bool parser(string txtScript, System.Windows.Forms.ListBox errout)
+        { 
+            
+            string command = txtScript.Trim().ToLower();
+            string[] lines = command.Split("\r");
+            foreach (string line in lines)
+            {
+                string formatLine = line.Replace("\n", "");
+
+                base.parser(formatLine, errout);
+               
+            }
+            
+            return true;
+        }
+
     }
 }
