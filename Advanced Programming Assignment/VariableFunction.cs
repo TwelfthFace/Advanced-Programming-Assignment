77a8b85ee909f08f7315142bca79a4be4ee30a7f
TwelfthFace <alcomeho@gmail.com>
2022-01-04 19:02:14 +0000

    Variable functionality fully implemented and working.

--- Advanced Programming Assignment/VariableFunction.cs
+++ Advanced Programming Assignment/VariableFunction.cs
@@ -1,17 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 namespace Advanced_Programming_Assignment
 {
     public class VariableFunction : Function
     {
-        Dictionary<string,string> keyValuePairs = new Dictionary<string,string>();
+       protected Dictionary<string,string> keyValuePairs = new Dictionary<string,string>();
 
         public override bool run(string parameters)
         {
             throw new NotImplementedException();
+        }
+
+        public string[] getKeys()
+        {
+            List<string> keys = new List<string>();
+            foreach (string key in keyValuePairs.Keys)
+            {
+                keys.Add(key);
+            }
+            return keys.ToArray();
+        }
+
+        public void clearKeys()
+        {
+            keyValuePairs.Clear();
         }
 
         public string findValue(string key)
@@ -21,12 +37,22 @@ namespace Advanced_Programming_Assignment
             return value;
         }
 
+        public string substituteValues(string command, string value)
+        {
+            //bool regexCommand = Regex.IsMatch(command, @"\b(" + value + @")\b");
+            string regexCommand = Regex.Replace(command, @"\b(" + value + @")\b", findValue(value), RegexOptions.IgnorePatternWhitespace, TimeSpan.FromSeconds(.25));
+            return regexCommand;
+        }
+
         public override void enumerateCommands(string parameters)
         {
             string[] keyValue = parameters.Split("=");
             string key = keyValue[0].Trim();
             string value = keyValue[1].Trim();
-            keyValuePairs.Add(key, value);
+            if (!keyValuePairs.ContainsKey(key))
+            {
+                keyValuePairs.Add(key, value);
+            }
         }
 
         public VariableFunction(ListBox errBox, Canvas canvas) : base(errBox, canvas){}
